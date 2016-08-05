ca8092025d5e3484461dfba96346597d14dfb6a4
Ivan Smelchakov <ivanxp@yandex.com>
2016-08-05 14:49:56 +0500

    Релиз

---
+++ 2gisdialer-cli.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+read -p "Телефон: " phone
+curl -s "http://apps.2gis.ru/phone/$phone"|grep "var data"|sed s/,\"/\\n/g|sed 's/\        var data = {"formattedPhone"/Номер/'|grep Номер
+curl -s "http://apps.2gis.ru/phone/$phone"|grep "var data"|sed s/,\"/\\n/g|sed 's/region"/Регион/'|grep Регион
+curl -s "http://apps.2gis.ru/phone/$phone"|grep "var data"|sed s/,\"/\\n/g|sed 's/company"/Компания/'|grep Компания
+curl -s "http://apps.2gis.ru/phone/$phone"|grep "var data"|sed s/,\"/\\n/g|sed 's/blockedInfo"/Жалобы/'|grep Жалобы
+
+
+
+
+
+
