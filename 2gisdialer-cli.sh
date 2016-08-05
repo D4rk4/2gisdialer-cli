4fc0d1e3e56bc2eb7736c4650491f92477c193fd
Ivan Smelchakov <ivanxp@yandex.com>
2016-08-05 15:03:00 +0500

    msg 'Ничего не нашлось'

--- 2gisdialer-cli.sh
+++ 2gisdialer-cli.sh
@@ -4,8 +4,7 @@ curl -s "http://apps.2gis.ru/phone/$phone"|grep "var data"|sed s/,\"/\\n/g|sed '
 curl -s "http://apps.2gis.ru/phone/$phone"|grep "var data"|sed s/,\"/\\n/g|sed 's/region"/Регион/'|grep Регион
 curl -s "http://apps.2gis.ru/phone/$phone"|grep "var data"|sed s/,\"/\\n/g|sed 's/company"/Компания/'|grep Компания
 curl -s "http://apps.2gis.ru/phone/$phone"|grep "var data"|sed s/,\"/\\n/g|sed 's/blockedInfo"/Жалобы/'|grep Жалобы
-
-
+curl -s "http://apps.2gis.ru/phone/79227156410"|grep "Ничего не нашлось"|sed 's/<title>//'|sed 's/<\/title>//'|sed 's/\        //'
 
 
 
